b5f78c8e609887e84a5a2040faff2c123553595a
Ekaterina Rybalka <mokosha@yandex-team.ru>
2019-07-18 19:57:56 +0500

    Инициализация приложения для VK бота

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const VkBot = require('node-vk-bot-api');
+
+const app = express();
+const jsonParser = bodyParser.json();
+
+const bot = new VkBot({ token: process.env.TOKEN });
+
+bot.on((ctx) => {
+    ctx.reply('Hello!');
+});
+
+app.use(jsonParser);
+
+app.post('/', bot.webhookCallback);
+
+
+module.exports = app;
